e0cc08d759cba728f122ba68ac623856504b9246
Alexander Winter <a.w1nter@hotmail.com>
2026-03-05 22:36:42 -0500

    --dry-run finally all working

--- init.sh
+++ init.sh
@@ -9,17 +9,22 @@ function winterfmt() {(
 		return
 	fi
 
-	cmdopts="$(echo "$@" | grep -oE "\-([A-Za-z0-9]|-|=)+")"
-	if [ -n "$cmdopts" ]; then
-		cmdopts="$cmdopts"
-	fi
+	cmdopts=$(grep -oE "\-([A-Za-z0-9]|-|=)+" <<< $@)
 
 	set -e
-	clang-format --style=file:$WINTERFMT_FILE $cmdopts "$@"
+	cmd="clang-format --style=file:$WINTERFMT_FILE $@ 2>&1"
+	output=$(script -q -c $cmd /dev/null)
 	if [[ $cmdopts =~ "-i" ]]; then
 		perl -g -i -pe 's/(catch\s*\(([A-Za-z0-9]|\|\.|\s)+ignored\)(\s*\n*)*)\{\s+\}/\1\{\}/igs' "$@"
 	elif [[ $cmdopts =~ "-n" ]] || [[ $cmdopts =~ "--dry-run" ]]; then
-
+		unwanted=$(grep catch -C 1 <<< $output)
+		output=$(grep -vF $unwanted <<< $output)
+	fi
+	output=$(sed 's/^\s*\|\s*$//g' <<< $output)
+	colorless=$(sed 's/\x1B\[[0-9;]\{1,\}[A-Za-z]//g' <<< $output)
+	if [[ $colorless =~ [^[:space:]] ]]; then
+		echo $output
+		return 1
 	fi
 )}
 
